3736ba726eb1a96aeb2ab8ea443d4aed5121cf4e
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-23 19:14:14 -0300

    Exercício #3

---
+++ exercices/introducao-a-javascript-e-logica-de-programacao_1/exercicio_3.js
@@ -0,0 +1,14 @@
+/*
+Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados. 
+*/
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (a > b) {
+    console.log(a);
+} else if (b > c) {
+    console.log(b);
+} else {
+    console.log(c);
+}
