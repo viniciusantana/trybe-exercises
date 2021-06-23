257730a4a2c396c834d9078469abeca9a1aa529f
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-23 15:20:40 -0300

    Exercícios do conteúdo finalizado

---
+++ bloco_4/dia_1/conteudo_1/parte3.js
@@ -0,0 +1,27 @@
+/*
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientInfo);
+-----------------------------------------
+ Agora que você já conhece os operadores aritméticos básicos do JavaScript, vamos praticá-los? Você pode consultar a lista de operadores na tabela JavaScript Arithmetic Operators disponível nesse link se tiver dúvidas. Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+
+    Crie uma variável chamada base e uma variável chamada altura e atribua os seus respectivos valores: 5 e 8;
+    Crie uma variável chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+    Crie uma variável chamada perimetro e atribua a ela a soma de todos os lados do retângulo;
+
+*/
+var base = 5;
+var altura = 8;
+var area = base * altura;
+
+console.log (area);
+
+var perimetro = (base + altura) * 2;
+
+console.log (perimetro);
