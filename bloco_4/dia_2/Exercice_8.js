37192b3a3edf730de69016cb93dbc20a5cb22768
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-25 11:49:34 -0300

    Exercício #8

---
+++ bloco_4/dia_2/Exercice_8.js
@@ -0,0 +1,9 @@
+//Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado; 
+
+let numbers = [];
+
+for (let index = 0; index < 25; index++){
+    numbers[index] = index + 1 ;
+    //console.log(numbers[index]);
+}
+console.log(numbers);
