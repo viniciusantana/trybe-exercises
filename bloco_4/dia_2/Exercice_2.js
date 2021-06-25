827346207e7a12bfe026a4634e1bd4a56c43f3ca
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-25 11:19:30 -0300

    Exercício #2

--- bloco_4/dia_2/Exercice_2.js
+++ bloco_4/dia_2/Exercice_2.js
@@ -3,4 +3,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /*
 Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
 */
+let somaArray = 0;
 
+for (let index = 0; index < numbers.length; index++) {
+    somaArray += numbers[index];
+}
+console.log(somaArray);
+
+/*
+//Outra forma
+
+for(let index of numbers){
+    somaArray += index;
+}
+console.log(somaArray);
+
+*/
