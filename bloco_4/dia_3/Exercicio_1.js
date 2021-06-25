448e37c552d401390cef03447c737f878af6bb91
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-25 15:53:53 -0300

    Exercício #1

---
+++ bloco_4/dia_3/Exercicio_1.js
@@ -0,0 +1,38 @@
+/*
+ 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+
+let n = 5;
+
+for (let index = 0; index < n; index++) {
+    let strings = '';
+    for (let indexx = 0; indexx < n; indexx++) {
+        strings += '*';
+    }
+    console.log(strings);
+}
+
+/*
+Gabarito
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
+*/
