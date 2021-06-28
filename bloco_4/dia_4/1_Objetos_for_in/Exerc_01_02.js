59dd2980cd97df9aad1e8738800239993d767323
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-28 19:24:27 -0300

    Parte_1/Exercício #4

--- bloco_4/dia_4/1_Objetos_for_in/Exerc_01_02.js
+++ bloco_4/dia_4/1_Objetos_for_in/Exerc_01_02.js
@@ -18,3 +18,35 @@ for(let key in info){
   console.log(key);
 }
 
+//4.Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. 
+console.log('----EXERCÍCIO 4----')
+for(let key in info){
+  console.log(info[key]);
+}
+
+//5.Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 
+//'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 
+//'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
+console.log('----EXERCÍCIO 5----')
+let infoB = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+
+for(let key in info){
+  if(info[key] !== infoB[key]){
+    console.log(info[key] + ' e ' + infoB[key]);
+  }else{
+    console.log('Ambos recorrentes // Atenção para essa última linha!')
+  }
+}
+/*
+Valor esperado:
+  Margarida e Tio Patinhas
+  Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+  Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+  Ambos recorrentes // Atenção para essa última linha!
+*/
