65f1ca929f7b2cd721be083c77676b637b3584a0
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-29 10:51:10 -0300

    Bonus/Exercício #1 + Explicado

--- bloco_4/dia_4/3_bonus/Exercicio_01.js
+++ bloco_4/dia_4/3_bonus/Exercicio_01.js
@@ -23,12 +23,13 @@
     | M   | 1000 |
 
     Que tal criar um objeto que associe cada letra a um numeral para fácil consulta?
-    Atenção! Quando você tem um número pequeno à direita de um número grande, eles devem ser somados. Exemplo: XI = 10 + 1 = 11. No entanto, se o número pequeno está à esquerda de um número maior que ele, ele deve ser subtraído. Exemplo: IX = 10 - 1 = 9.
+    Atenção! Quando você tem um número pequeno à direita de um número grande, eles devem ser somados.
+     Exemplo: XI = 10 + 1 = 11. No entanto, se o número pequeno está à esquerda de um número maior que ele, ele deve ser subtraído. Exemplo: IX = 10 - 1 = 9.
 */
 
 
 
-function converteRomano (emRomano){
+function converteRomano(emRomano) {
     let romanos = {
         I: 1,
         V: 5,
@@ -38,22 +39,24 @@ function converteRomano (emRomano){
         D: 500,
         M: 1000,
     }
-    let emNumeros =[];
+    let emNumeros = [];
     let resultado = 0;
-    for(index = 0; index < emRomano.length; index++){
-        for(let key in romanos){
-            if(key === emRomano[index]){
-                emNumeros[index]= romanos[key];
+    //Converte as letras romanas separadamente em números! 
+    for (index = 0; index < emRomano.length; index++) {
+        for (let key in romanos) {
+            if (key === emRomano[index]) {
+                emNumeros[index] = romanos[key];
             }
             //console.log(key, emRomano[index], romanos[key]); //Teste
         }
         //console.log(emNumeros[index]); //Teste
     }
-    for(somaIndex = 0; somaIndex < emNumeros.length; somaIndex++ ){
+    //Verifica se a letra é um número de incremento ou decremento e soma.
+    for (somaIndex = 0; somaIndex < emNumeros.length; somaIndex++) {
 
-        if(emNumeros[somaIndex] < emNumeros[somaIndex + 1]){
+        if (emNumeros[somaIndex] < emNumeros[somaIndex + 1]) {
             resultado -= emNumeros[somaIndex];
-        }else{
+        } else {
             resultado += emNumeros[somaIndex];
         }
         //console.log(resultado); //Teste
@@ -62,3 +65,5 @@ function converteRomano (emRomano){
 }
 
 console.log(converteRomano('MCMXCII'));
+
+
