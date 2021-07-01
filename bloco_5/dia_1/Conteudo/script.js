346421e0fb0bafc75f3b2c5250999232c4c66fde
viniciussantana <viniciusantana@yahoo.com.br>
2021-06-30 22:30:26 -0300

    Exercício do conteúdo

---
+++ bloco_5/dia_1/Conteudo/script.js
@@ -0,0 +1,11 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.getElementById('footer-container').style.backgroundColor = 'black';
+document.querySelector('.emergency-tasks div h3').style.backgroundColor = 'blue';
+document.querySelector('.no-emergency-tasks div h3').style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = 'black';
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = 'blue';
+//document.getElementsByTagName('section').style.backgroundColor = 'green'; //<- Não sei pq não funciona!
+document.querySelectorAll('section')[0].style.backgroundColor = 'yellow';
+document.querySelectorAll('section')[1].style.backgroundColor = 'yellow';
+
+//emergencial = ba
