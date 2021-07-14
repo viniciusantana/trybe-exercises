c9b0a2a6539d3532c3a8dce263a0748708d51ab0
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-14 10:01:09 -0300

    Finalizando

--- bloco_5/dia_3/Exercício/script.js
+++ bloco_5/dia_3/Exercício/script.js
@@ -138,10 +138,29 @@ function noZoom(event){
 */
 
 function tarefas (tarefaString) {
+    let myTask = document.querySelector('.my-tasks');
     let spanTarefa = document.createElement('span');
     spanTarefa.innerHTML = tarefaString;
-    let myTask = document.querySelector('.my-tasks');
     myTask.appendChild(spanTarefa);
 }
 tarefas('Estudar');
+
+/*
+Exercício 8:
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada
+no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor")
+e criar dinamicamente um elemento de tag <div> com a classe task .
+
+>O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+>O elemento criado deverá ser adicionado como filho/filha da tag <div> 
+ que possui a classe "my-tasks" .
+*/
+function makeSub (cor){
+    let myTask = document.querySelector('.my-tasks');
+    const createDiv = document.createElement('div');
+    createDiv.style.backgroundColor = cor;
+    createDiv.className = 'task';
+    myTask.appendChild(createDiv);
+}
+makeSub('green');
 
