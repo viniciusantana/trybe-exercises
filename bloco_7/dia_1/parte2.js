2d6e13004487db964a077ba1233fae525a8eafbc
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-22 20:56:40 -0300

    part 2

--- bloco_7/dia_1/parte2.js
+++ bloco_7/dia_1/parte2.js
@@ -16,5 +16,5 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(`Os números ${oddsAndEvens.sort((a , b) => a - b)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
 
