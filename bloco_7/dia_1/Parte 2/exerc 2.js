fb486f455ad975be78f4a1f6689045cf098e2168
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-23 11:26:33 -0300

    organiza parte 1(1 e 2), conclui parte 2 (exercice 1)

---
+++ bloco_7/dia_1/Parte 2/exerc 2.js
@@ -0,0 +1,14 @@
+/* 
+    Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+    Exemplo:
+    longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+*/
+
+const longestWord = (x) => {
+    let strings = x.split(' ');
+    for (let index of strings) {
+        
+    }
+    
+}
