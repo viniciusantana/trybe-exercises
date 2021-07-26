b2d4476dcfbb798755dd86d4642c32fe7308f7e6
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-26 20:01:33 -0300

    parte 1 ex2 e gabarito

--- bloco_7/dia_3/parte_1/ex2.js
+++ bloco_7/dia_3/parte_1/ex2.js
@@ -30,6 +30,18 @@ assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 3, 4 ]);
 // D
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [ 1, 2, 3, 4 ]);
 
+//GABARITO:
+/*
+
+assert.strictEqual(typeof myRemove, 'function');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const myList = [5, 6, 7, 8];
+myRemove(myList, 5);
+assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
 
-
+ */
