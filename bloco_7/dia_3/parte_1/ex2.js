340698701a538550247f1607238edb3f84c81d9c
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-26 19:21:19 -0300

    parte 1 ex2-b

--- bloco_7/dia_3/parte_1/ex2.js
+++ bloco_7/dia_3/parte_1/ex2.js
@@ -1,9 +1,9 @@
 /* 
     A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
-    Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-    Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-    Verifique se o array passado por parâmetro não sofreu alterações
-    Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+    a-Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+    b- Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+    c-Verifique se o array passado por parâmetro não sofreu alterações
+    d-Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
  */
 const assert = require('assert');
 
@@ -20,3 +20,5 @@ function myRemove(arr, item) {
 
     // implemente seus testes aqui
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 4 ]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [ 1, 2, 3, 4 ]);
+
