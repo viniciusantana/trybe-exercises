c7daf7ebe582b01e5219ecb6bf792702a512681c
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-26 21:59:48 -0300

    parte 2 ex3

--- bloco_7/dia_3/parte_2/ex3.js
+++ bloco_7/dia_3/parte_2/ex3.js
@@ -2,6 +2,13 @@
 
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
+const sumAllNumbers = (allNumbers) => {
+    let sumInput = 0;
+    for(let index = 0; index < allNumbers.length; index += 1) {
+        sumInput += allNumbers[index];
+    }
+    return sumInput;
+}
 
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
