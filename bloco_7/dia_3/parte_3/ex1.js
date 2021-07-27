00e2be199da00cd92fbccbd0d7f326a30ee22ff5
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-26 22:45:45 -0300

    parte 3 ex1 adiciona gabarito

--- bloco_7/dia_3/parte_3/ex1.js
+++ bloco_7/dia_3/parte_3/ex1.js
@@ -25,3 +25,23 @@ const parameter = ['Irina', 'Ashleigh', 'Elsa'];
 const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 
 assert.deepStrictEqual(greetPeople(parameter), result);
+
+/*
+
+const greetPeopleNew = (people) => {
+  const greeting = [];
+  for (const person in people) {
+    greeting.push(`Hello ${people[person]}`);
+  }
+  return greeting;
+};
+
+const assert = require('assert');
+assert.strictEqual(typeof greetPeopleNew, 'function');
+
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+const output = greetPeopleNew(parameter);
+assert.deepStrictEqual(output, result);
+*/
