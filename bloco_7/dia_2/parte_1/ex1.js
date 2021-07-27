7be0858d267c5f28dcc8dd073302768c794354ec
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-27 08:14:12 -0300

    parte_1 Ex_01 Abertura

---
+++ bloco_7/dia_2/parte_1/ex1.js
@@ -0,0 +1,64 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+        street: 'Rua das Flores',
+        number: '389',
+        apartment: '701',
+    },
+    order: {
+        pizza: {
+            marguerita: {
+                amount: 1,
+                price: 25,
+            },
+            pepperoni: {
+                amount: 1,
+                price: 20,
+            }
+        },
+        drinks: {
+            coke: {
+                type: 'Coca-Cola Zero',
+                price: 10,
+                amount: 1,
+            }
+        },
+        delivery: {
+            deliveryPerson: 'Ana Silveira',
+            price: 5,
+        }
+    },
+    payment: {
+        total: 60,
+    },
+};
+
+const customerInfo = (order) => {
+    // Adicione abaixo as informações necessárias.
+
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+
+}
+
+orderModifier(order);
+
+/* 
+Agora você vai fazer alguns exercícios de fixação.
+
+Complete a função customerInfo() para que seu retorno seja similar a:
+"Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+
+Note que o parâmetro da função já está sendo passado na chamada da função.
+
+Complete a função orderModifier() para que seu retorno seja similar a:
+"Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+
+Modifique o nome da pessoa compradora.
+Modifique o valor total da compra para R$ 50, 00.
+ */
