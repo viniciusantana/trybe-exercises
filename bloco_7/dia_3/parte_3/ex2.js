f64dcfe8cfa37807e13cfd94c90cdda4df12c9fc
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-26 23:39:15 -0300

    parte 3 ex2 fim

--- bloco_7/dia_3/parte_3/ex2.js
+++ bloco_7/dia_3/parte_3/ex2.js
@@ -2,11 +2,12 @@
 escreva testes para verificar se a mesma está retornando como se vê na variável result e,
 caso não esteja, altere o código para que ele passe nos testes. 
  */
-
+const assert = require('assert');
 
 const removeVowels = (word) => {
     const characters = word.split('');
     const results = [];
+    let counter = 1;
   
     for (let index = 0; index < characters.length; index += 1) {
       if (
@@ -16,14 +17,16 @@ const removeVowels = (word) => {
         characters[index] === 'e' ||
         characters[index] === 'u'
       ) {
+          results.push(`${counter}`);
+          counter += 1;
+    } else {
         results.push(characters[index]);
-      } else {
-        results.push('_');
       }
     }
-    return results;
+    return results.join('');
   };
   
   
   const parameter = 'Dayane';
   const result = 'D1y2n3';
+  assert.strictEqual(removeVowels(parameter), result);
