59de4e35efdc8ee92db51799fc2f6d3f1b8e06b5
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-26 21:07:54 -0300

    parte 1 ex3 Gabarito e identação

--- bloco_7/dia_3/parte_1/ex3.js
+++ bloco_7/dia_3/parte_1/ex3.js
@@ -6,28 +6,38 @@
     D.Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
  */
 
-    const assert = require('assert');
+const assert = require('assert');
 
-    function myRemoveWithoutCopy(arr, item) {
-      for (let index = 0, len = arr.length; index < len; index += 1) {
+function myRemoveWithoutCopy(arr, item) {
+    for (let index = 0, len = arr.length; index < len; index += 1) {
         if (arr[index] === item) {
-          arr.splice(index, 1);
-          index -= 1;
-          len -= 1;
+            arr.splice(index, 1);
+            index -= 1;
+            len -= 1;
         }
-      }
-    
-      return arr;
     }
-    
-    // implemente seus testes aqui
 
-    assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
-    assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
-    assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+    return arr;
+}
 
-    let arrayz = [5, 6, 7, 8];
-    myRemoveWithoutCopy(arrayz, 7);
-    assert.deepStrictEqual(arrayz, [5, 6, 8]);
+// implemente seus testes aqui
 
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
+let arrayz = [5, 6, 7, 8];
+myRemoveWithoutCopy(arrayz, 7);
+assert.deepStrictEqual(arrayz, [5, 6, 8]);
+
+//Gabarito:
+
+const myList = [1, 2, 3, 4];
+
+/* assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+myRemoveWithoutCopy(myList, 1);
+assert.strictEqual(myList.length, 3);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]); */
