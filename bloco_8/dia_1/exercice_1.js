89d5f0976f419965d61c235d6cc106d974e0f42b
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-02 20:58:18 -0300

    dia1/exercicio1

--- bloco_8/dia_1/exercice_1.js
+++ bloco_8/dia_1/exercice_1.js
@@ -4,12 +4,39 @@ Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa co
 A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um 
 email no formato nome_da_pessoa@trybe.com . 
  */
+const newJob = (name) => {
 
-const newEmployees = () => {
+    const email = `${name.replace(/\s/g, '_')}@trybe.com`;
+    return {fullName: name, email: email};
+}
+console.log(newJob('aa aaa aa'));
+
+const newEmployees = (callback) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callback('Pedro Guerra').fullName, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Pedro Guerra').fullName, // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Pedro Guerra').fullName, // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+  console.log(newEmployees(newJob));
+
+  // Modo 1
+  /*
+  const newJob = (name) => {
+
+    const email = `${name.replace(/\s/g, '_')}@trybe.com`;
+    return {fullName: name, email: email};
+}
+console.log(newJob('aa aaa aa'));
+
+const newEmployees = (user1, user2, user3) => {
+    const employees = {
+      id1: user1.fullName, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: user2.fullName, // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: user3.fullName, // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+  console.log(newEmployees(newJob('Pedro Guerra'), newJob('Luiza Drumond'), newJob('Carla Paiva')));
+*/
