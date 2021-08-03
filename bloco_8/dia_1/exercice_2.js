880fe5164df56f155df37001cd9d0e022af27bdb
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-02 21:38:28 -0300

    dia1/exercicio2 + Gabarito conferido OK

--- bloco_8/dia_1/exercice_2.js
+++ bloco_8/dia_1/exercice_2.js
@@ -10,3 +10,16 @@ const resultadoSorteio = (numApostado, verificador) => {
   return verificador(numSorteado, numApostado)? "Parabéns você ganhou": "Tente novamente";
 }
 console.log(resultadoSorteio(4, verificador));
+
+//Gabarto site:
+/*
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
+*/
