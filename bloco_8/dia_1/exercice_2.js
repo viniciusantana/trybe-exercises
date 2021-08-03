78bc86dc5d40c93a195eab514c52d05b5035e754
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-02 21:36:49 -0300

    dia1/exercicio2

--- bloco_8/dia_1/exercice_2.js
+++ bloco_8/dia_1/exercice_2.js
@@ -4,3 +4,9 @@ Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros
 e uma função que checa se o número apostado é igual ao número sorteado. 
 O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). 
 */
+const verificador = (value1, value2) => value1 === value2;
+const resultadoSorteio = (numApostado, verificador) => {
+  const numSorteado = Math.floor(Math.random() * (6));
+  return verificador(numSorteado, numApostado)? "Parabéns você ganhou": "Tente novamente";
+}
+console.log(resultadoSorteio(4, verificador));
