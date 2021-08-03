8056a141c7d76fe1b65efa6c37ec9ea5d34863b3
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-02 22:08:31 -0300

    dia1/exercicio3

--- bloco_8/dia_1/Exercice_3.js
+++ bloco_8/dia_1/Exercice_3.js
@@ -12,3 +12,20 @@ e quando não houver resposta ("N.A") não altera-se a contagem.
 
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const score = (rightAnswers, studentAnswer) => {
+    let accumulator = 0;
+    for (let index = 0; index < rightAnswers.length; index++) {
+        if(rightAnswers[index] === studentAnswer[index]){
+            accumulator++;
+        }else {
+            accumulator -= (studentAnswer[index] === 'N.A'? 0 : 0.5);   
+        }
+    }
+    return accumulator;
+}
+
+const hof = (rightAnswers, studentAnswer, callback) => {
+    return callback( rightAnswers, studentAnswer);
+}
+console.log(hof(RIGHT_ANSWERS, STUDENT_ANSWERS, score));
