b6e10319c2498bf3fe6792f3c941ad99bef93559
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-03 08:56:57 -0300

    dia2/exercicio7 correto mas precisa refatorar

--- bloco_8/dia_2/exerc_07.js
+++ bloco_8/dia_2/exerc_07.js
@@ -73,17 +73,16 @@ const expectedResult = false;
 
 function authorUnique() {
   // escreva seu código aqui
-  books.some((value) => {
+  return !(books.some((value) => {
     const atual = value;
-    return books.some((value2) => {
+    return (books.some((value2) => {
       if(atual.id !== value2.id){
-        return atual.author.birthYear === value2.author.birthYear;
+        return (atual.author.birthYear === value2.author.birthYear);
       }else {
         return false;
-
       }
-    })
-  })
+    }))
+  }));
 }
 
 assert.strictEqual(authorUnique(), expectedResult);
