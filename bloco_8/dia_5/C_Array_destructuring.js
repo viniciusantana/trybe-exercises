3aa90facd3ad4cc74fff09420f08695613dd1431
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-04 10:16:31 -0300

    conteúdo Array destructuring 2

--- bloco_8/dia_5/C_Array_destructuring.js
+++ bloco_8/dia_5/C_Array_destructuring.js
@@ -10,4 +10,18 @@ const [msg, printf] = saudacoes;
 printf(msg);
 printf('--------');
 
+//2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis. 
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida); // arroz gato água
+
+printf('--------');
 
