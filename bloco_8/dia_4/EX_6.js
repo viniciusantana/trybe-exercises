54769098727d2caf8a66083b0c419e7b0307f655
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-04 08:46:53 -0300

    dia4/ex_06

--- bloco_8/dia_4/EX_6.js
+++ bloco_8/dia_4/EX_6.js
@@ -13,6 +13,12 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
   // escreva seu código aqui
+  return students.map((value, index) => (
+    {
+      name: value,
+      average: grades[index].reduce((acc, atual) => (acc + atual)) / 5,
+    }
+  ));
 }
 
 const expected = [
