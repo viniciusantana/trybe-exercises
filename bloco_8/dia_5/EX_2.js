7c0d750925c13bdcaf9329a8e4a9406172835033
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-09 14:30:51 -0300

    dia5/EX_02

---
+++ bloco_8/dia_5/EX_2.js
@@ -0,0 +1,14 @@
+/* Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+
+    Dica: use parâmetro rest .
+ */
+const assert = require('assert');
+
+// escreva sum abaixo
+const sum = (...num) => (num.reduce((acumulator, atuaNum) => (acumulator + atuaNum), 0));
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
