0a5341c8a38de1d9d5cb03a8b6d187dba7c3cf84
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-12 08:53:56 -0300

    exercicio 2

--- bloco_9/dia_2/EX_01.js
+++ bloco_9/dia_2/EX_01.js
@@ -17,7 +17,24 @@ const fetchJoke = () => {
 const addJokeContainer = (string) => {
   const jokeContainer = document.querySelector('#jokeContainer');
   jokeContainer.innerHTML = string;
-}
+};
 
+const firstPromise = new Promise((resolve, reject) => {
+  let arrayNumeber = [];
+  let acc = 0;
+  for (let index = 0; index < 10; index++) {
+    let randomNumber = Math.floor(Math.random() * (50 - 1)) + 1;
+    arrayNumeber.push(randomNumber * randomNumber);
+    acc += arrayNumeber[index];
+  }
+  if( acc > 8000){
+    reject(acc);
+  } 
+  resolve(acc);
+})
+.then((value) => console.log(`Promise resolvida com valor ${value} `))
+.catch((value) => console.log(`Promise ${value} Falhou`));
+
+// firstPromise();
 
 window.onload = () => fetchJoke();
