f098f337efb8fb3e6a67b80d27c3e8ded510e1a5
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-17 19:50:13 -0300

    Parte_1/Exercício_2

---
+++ bloco_10/dia_01/Parte_01/Ex_02.test.js
@@ -0,0 +1,26 @@
+const myRemove = require('./Ex_02');
+
+
+/* A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+Verifique se o array passado por parâmetro não sofreu alterações
+Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+ */
+
+test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1,2,4]);
+});
+
+test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+/* //NÂO ENTENDI
+test('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+ */
+test('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+});
