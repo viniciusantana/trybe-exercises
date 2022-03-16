43df22a64e5f35bc96fcba0e5012189da1102127
viniciusantana <viniciusantana@yahoo.com.br>
2022-03-16 10:46:46 -0300

    feat: req. 3 e 4

--- bloco_22/dia_01/Exercicios/imc.js
+++ bloco_22/dia_01/Exercicios/imc.js
@@ -1,8 +1,16 @@
-/* 
-Crie um script para calcular o Índice de Massa Corporal(IMC) de uma pessoa. 
- */
+// https://www.npmjs.com/package/readline-sync#utility_methods
+const readline = require('readline-sync');
+ 
+// 1. Crie um script para calcular o Índice de Massa Corporal(IMC) de uma pessoa. 
+
 const imc = (peso, altura) => {
   return peso / altura ** 2;
 }
 // 2. Agora, permita que o script seja executado através do comando npm run imc
 console.log(imc(80, 1.67));
+
+// Chegou a hora de tornar nosso script mais interativo! Vamos adicionar input de quem usa. 
+const peso = readline.questionFloat('Qual seu peso? ');
+const altura = readline.questionFloat('Qual sua altura? ');
+
+console.log(imc(peso, altura));
